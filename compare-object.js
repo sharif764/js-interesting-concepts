1bf7e59cbdda6ef1130f3f5a5d758a44b7051291
sharif764 <ss1884088@gmail.com>
2021-08-31 01:27:15 +0600

    Added bind method file

--- compare-object.js
+++ compare-object.js
@@ -1,13 +1,32 @@
-const first = { a: 1, b: 2 };
-const second = { a: 1, b: 2 };
-const third = first;
-// console.log(JSON.stringify(first, second))
-if (JSON.stringify(first) === JSON.stringify(second)) {
-    console.log('equal');
-}
-else {
-    console.log('not equal');
-}
-function compareObjects(obj1, obj2) {
+// const first1 = { a: 1, b: 2 };
+// const second2 = { a: 1, b: 2 };
+// const third = first1;
+// // console.log(JSON.stringify(first, second))
+// // if (JSON.stringify(first) === JSON.stringify(second)) {
+// //     console.log('equal');
+// // }
+// // else {
+// //     console.log('not equal');
+// // }
+// function compareObjects(obj1, obj2) {
+//     if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+//         return false;
+//     }
+//     for (const prop in obj1) {
+//         if (obj1[prop] !== obj2[prop]) {
+//             return false;
+//         }
+//         return true;
+//     }
+// }
+// const isEual = compareObjects(first1, second2);
+// console.log(isEual);
 
-}
+let i = 1;
+// console.log(i);
+i = 2;
+// console.log(i);
+const name = { id: 1 };
+console.log(name.id);
+name.id = 2;
+console.log(name.id);
